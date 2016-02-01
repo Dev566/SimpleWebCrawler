834057803c3b052ce9ae6704b998d44589c3538a
Debabrata <dev.566@gmail.com>
2016-02-01 00:20:39 -0600

    Creating Repository

---
+++ WebCrawler/src/SpiderTest.java
@@ -0,0 +1,8 @@
+
+public class SpiderTest {
+	public static void main(String[] args)
+    {
+        Spider spider = new Spider();
+        spider.search("http://www.google.com/", "chart");
+    }
+}
